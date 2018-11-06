19646f72befeca28796bf8fdbaed62867fff554f
Ricardo Piras <ricpiras@gmail.com>
2018-11-06 17:32:43 +0900

    Added new fonts

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,12 +1,11 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :uptade, :destroy]
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
 
 def index
   @tasks = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
   end
 
   def new
@@ -23,11 +22,9 @@ def index
   end
 
   def edit
-    @task = Task.find(params[:id])
   end
 
   def update
-    @task = Task.find(params[:id])
     if @task.update(task_params)
       redirect_to task_path(@task)
     else
@@ -36,7 +33,6 @@ def index
   end
 
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
     redirect_to tasks_path
   end
@@ -46,6 +42,7 @@ def index
   def task_params
     params.require(:task).permit(:title, :details, :completed)
   end
+
   def set_task
     @task = Task.find(params[:id])
   end
